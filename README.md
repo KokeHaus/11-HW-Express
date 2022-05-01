25af480f20407ff7234e28b120cb429c830f9f72
KokeHaus <austinbcw@gmail.com>
2022-04-30 23:21:47 -0500

    Working Demo

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # 11-HW-Express
+![Github licence](https://img.shields.io/badge/License-MIT-yellow.svg)
+
+## Description 
+Express routing API calls for Note Taking app!
+ 
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+
+## Installation 
+Clone from github. Node and express required.
+
+## Usage 
+Deployed on github pages
+Start taking Notes!
+
+
+## License 
+This project is license under MIT.
+
+## Contributing 
+Contributors should read the installation section. 
+
+
--- server.js
+++ server.js
@@ -2,16 +2,68 @@
 const express = require("express");
 const fs = require("fs");
 const app = express();
+var path = require("path");
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-require('./routes/routes')(app);
 
+app.use(express.static("public"));
 
+    ////////////////////////////////////////////////////////////////////
+    app.get("/api/notes", function(req, res) {
+        fs.readFile(__dirname + "/Develop/db/db.json", 'utf8', function (err, data) {
+            if (err)
+              return console.error(err)
+            res.json(JSON.parse(data))
+          })
+    });
+    ////////////////////////////////////////////////////////////////////
+    app.post("/api/notes", function(req, res) {
+        fs.readFile(__dirname + "/Develop/db/db.json", 'utf8', function (err, data) {
+            if (err) 
+              return console.error(err)
+        notes = JSON.parse(data)
+        let id = notes.length;
+        let newNote = { title: req.body.title, text : req.body.text, id: id,}
+        let currNote = notes.concat(newNote);
+        update(currNote);
+        res.json(currNote);
+        })
 
-
-
+    });
+    ////////////////////////////////////////////////////////////////////
+    app.get("/api/notes/:id", function(req,res) {
+        res.json(notes[req.params.id]);
+    });
+    ////////////////////////////////////////////////////////////////////
+    app.delete("/api/notes/:id", function(req, res) {
+        const id = JSON.parse(req.params.id)
+        fs.readFile(__dirname + "/Develop/db/db.json", 'utf8', function (err, data) {
+            if (err) 
+              return console.error(err)
+        notes=JSON.parse(data);
+        notes = notes.filter(val => val.id !== id);
+        update(notes);
+        res.json(notes);
+        })
+    });
+    ////////////////////////////////////////////////////////////////////
+    app.get('/notes', function(req,res) {
+        res.sendFile(path.join(__dirname, "/public/notes.html"));
+    });
+    ////////////////////////////////////////////////////////////////////
+    app.get('*', function(req,res) {
+        res.sendFile(path.join(__dirname, "/public/index.html"));
+    });
+    ////////////////////////////////////////////////////////////////////
+    function update(notes) {
+        fs.writeFile(__dirname + "/Develop/db/db.json", JSON.stringify(notes,'\t'), err => {
+            if(err) 
+                return console.error(err);
+            return true;
+        });
+    }
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
